4c7b54df3ade7d1482832644af02687f9e2119a0
natainditama <natainditama@gmail.com>
2022-08-21 13:10:25 +0800

    feat: Implement logic auth user

---
+++ js/auth.js
@@ -0,0 +1,81 @@
+import { select } from "./utils.js";
+
+class Auth {
+  constructor() {
+    this.listUser = "list-user";
+    this.currentUser = "current-user";
+    this.users = this.getUsers(this.listUser);
+    this.profile = this.getUsers(this.currentUser)[0];
+    this.values = {
+      email: "",
+      password: "",
+    };
+  }
+  init() {
+    this.profile = this.getUsers(this.currentUser)[0];
+    if (!!this.profile) {
+      if (select("nav.is-auth")) select("nav.is-auth").style.display = "flex";
+      if (select(".user-email")) {
+        select(".user-email").textContent = this.profile.email;
+      }
+    } else {
+      if (select("nav.not-auth")) select("nav.not-auth").style.display = "flex";
+    }
+  }
+  login() {
+    this.values = this.getValues();
+    this.users = this.getUsers(this.listUser);
+    const user = this.users.find((user) => {
+      return (
+        user.email === this.values.email &&
+        user.password === this.values.password
+      );
+    });
+    if (user) {
+      this.profile = user;
+      this.setUsers(this.currentUser, [user]);
+      window.location.replace("/");
+    } else {
+      alert("Invalid user or password");
+    }
+  }
+  logout() {
+    this.profile = [];
+    localStorage.removeItem(this.currentUser);
+  }
+  register() {
+    this.values = this.getValues();
+    this.users = this.getUsers(this.listUser);
+    const userExists = this.users.filter((user) => {
+      return (
+        user.email === this.values.email && user.password === this.values.email
+      );
+    });
+
+    if (!!userExists) {
+      this.users.push(this.values);
+      this.profile = this.values;
+      console.log(this.values);
+      this.setUsers(this.currentUser, [this.values]);
+      this.setUsers(this.listUser, this.users);
+      window.location.replace("/");
+    } else {
+      alert("User is already exists");
+    }
+  }
+  getUsers(itemUser) {
+    return JSON.parse(window.localStorage.getItem(itemUser)) || [];
+  }
+  setUsers(itemUser, data) {
+    localStorage.setItem(itemUser, JSON.stringify(data));
+  }
+  getValues() {
+    const values = {
+      email: document.querySelector('[name="email"]').value || "",
+      password: document.querySelector('[name="password"]').value || "",
+    };
+    return values;
+  }
+}
+
+export default Auth;
