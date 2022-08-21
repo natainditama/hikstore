cbf070551e8596a5ab894c47a5e6d18a4856bf18
natainditama <natainditama@gmail.com>
2022-08-21 13:16:19 +0800

    chore: Implement form auth system

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import Auth from "./auth.js";
+import { select, addEvent } from "./utils.js";
+
+const auth = new Auth();
+auth.init();
+
+addEvent("submit", ".form-register", (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  formData = Object.fromEntries(formData);
+  if (formData.password === formData.confirm) {
+    auth.register();
+  } else {
+    alert("Passwords do not match");
+  }
+});
+
+addEvent("submit", ".form-login", (e) => {
+  e.preventDefault();
+  auth.login();
+});
+
+console.log("Loaded");
