e7e02620e53307bd6930d5e8dd556ad1bbb720aa
Nata Inditama <natainditama.dev@gmail.com>
2024-01-01 19:50:20 +0800

    style: configure tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import { Metadata } from 'next';
+import { cn } from "@/lib/utils";
 
-import './globals.css';
+import "./globals.css";
 
-import { Inter } from 'next/font/google';
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: `TypeScript starter for Next.js`,
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>{children}</body>
     </html>
   );
 }
